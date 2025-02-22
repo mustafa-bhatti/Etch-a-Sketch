89da0d5e272e5968d890e10352c06cd0da233bb2
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-23 00:34:36 +0500

    Fixed a bug where at high number boxes were overflowing

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ step4: assign them block class
 */
 
 const newBtn = document.querySelector(".new-btn");
-let numberGrids = 2;
+let numberGrids = 55;
 newBtn.addEventListener("click", () => {
     const userNumber = parseInt(prompt("Enter the number of blocks, (1-100). Defualt 16x16."));
     if (userNumber>0 && userNumber<=100) {
@@ -20,12 +20,13 @@ newBtn.addEventListener("click", () => {
 function createGrid(numberGrids) {
     let blockWidth = 1000/numberGrids;
     const gridContainer = document.querySelector(".block-container");
-    let totalNumber = numberGrids * numberGrids;
-    for (let i = 0;i < totalNumber;i++) {
+    for (let i = 0;i < numberGrids;i++) {
+        for (let j = 0;j < numberGrids;j++) {
         const block = document.createElement("div");
         block.classList.add("block");
         block.style.width = blockWidth + "px";
         gridContainer.appendChild(block);
+        }
     }
 }
 
