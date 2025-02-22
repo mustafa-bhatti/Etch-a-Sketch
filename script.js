392013595e3030c8469ff02b08028e451fe99b2b
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-22 23:59:46 +0500

    add btn for prompt

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 
+step 1: get number of blocks from user
+step2: multipliy them to get total blocks and create div of that class
+step3: divide the number of block with width of the container(1000) ot get the width of the block and then assign that width to the class property
+step4: assign them block class
+*/
+
+const newBtn = document.querySelector(".new");
